0f0c52cfa8ca9b3001bdb137aec53ed8e371438a
Sourav Das <souravdas4816@gmail.com>
2024-09-04 16:38:41 +0530

    +importing with heading row positioned in different position

--- app/Imports/FirstSheetImport.php
+++ app/Imports/FirstSheetImport.php
@@ -4,15 +4,26 @@ namespace App\Imports;
 
 use App\Models\Exhibition;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class FirstSheetImport implements ToModel
+class FirstSheetImport implements ToModel, WithHeadingRow
 {
+    public function headingRow(): int
+    {
+        return 2;   // heading starts on column position B
+    }
+
+    public function startRow(): int
+    {
+        return 2;   // heading starts on row position 2
+    }
+
     public function model(array $row)
     {
         return new Exhibition([
-            'section'   => $row[0],
-            'country'   => $row[1],
-            'award'     => $row[2],
+            'section'   => $row['section'],
+            'country'   => $row['country'],
+            'award'     => $row['award'],
         ]);
     }
 }
